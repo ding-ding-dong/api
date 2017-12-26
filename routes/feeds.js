50feab9b491a200ada1cb0ed0ca49a9bf68bebc2
Mystist <gjl87910lq@gmail.com>
2017-12-26 14:54:54 +0800

    feat: support query by date and key

--- routes/feeds.js
+++ routes/feeds.js
@@ -6,16 +6,24 @@ import { getAll, getByTimestamp } from '../src/feeds'
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json(await getAll())
-})
+  let feeds = null
+  const date = req.query.date
 
-router.get('/:date', async (req, res, next) => {
-  const date = req.params.date
-  const currentDateString = moment.utc(date).utcOffset('+0800').format('YYYYMMDD')
-  const eDate = moment.utc(currentDateString).add(-8, 'hours').valueOf()
-  const sDate = moment.utc(eDate).add(-1, 'day').valueOf()
+  if (date) {
+    const currentDateString = moment.utc(date).utcOffset('+0800').format('YYYYMMDD')
+    const eDate = moment.utc(currentDateString).add(-8, 'hours').valueOf()
+    const sDate = moment.utc(eDate).add(-1, 'day').valueOf()
+    feeds = await getByTimestamp(sDate, eDate)
 
-  res.json(await getByTimestamp(sDate, eDate))
+    const key = req.query.key
+    if (key) {
+      feeds = feeds.filter(feed => feed.source.key === key)
+    }
+  } else {
+    feeds = await getAll()
+  }
+
+  res.json(feeds)
 })
 
 export default router
