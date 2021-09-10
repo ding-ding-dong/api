9e4eda074241ab1cffaf275ee40fce23853be353
Mystist <gjl87910lq@gmail.com>
2021-09-10 15:06:03 +0800

    testing

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /etc/init.d/ && ./redis_6379 start
+cd /etc/init.d/ && exec ./redis_6379 start
 
 exec /usr/bin/node --experimental-modules --loader /app/bin/custom-loader.mjs /app/bin/www
